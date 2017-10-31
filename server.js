233a250d5d41fcd9800c34a77fd08099bb9eeecf
Mike Heath <msheath@btinternet.com>
2017-10-31 21:11:55 +0000

    Correction of concatenation code

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var middleware = {
 		next();
 	},
 	logger: function(req, res, next) {
-		console.log('Request: '+new Date().toString()+' '+req.method+' '+req.originalUrl);
+		console.log('Request: '+ new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
 		next();
 	}
 };
@@ -19,8 +19,8 @@ app.get('/about', middleware.requireAuthentication, function(req, res) {
 	res.send('About us!');
 });
 
-app.use(express.static(__dirname+ '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function() {
-	console.log('server using port '+port);
+	console.log('server using port '+ port);
 });
